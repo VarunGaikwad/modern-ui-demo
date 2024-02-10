91cfc2997f5f3145fcee69f8c79811385fbb132f
Varun Gaikwad <gaikwadvarun23@gmail.com>
2024-02-10 18:55:42 +0530

    Restart

--- src/main.tsx
+++ src/main.tsx
@@ -2,26 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import Home from "./pages/Home.tsx";
-import History from "./pages/History.tsx";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<App />}>
-      <Route path="" element={<Home />}></Route>
-      <Route path="/history" element={<History />}></Route>
-    </Route>
-  )
-);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
